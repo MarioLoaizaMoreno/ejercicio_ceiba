014000d1a65aae637432c4794b28f007bd06003f
Your Name <you@example.com>
2025-09-14 11:32:10 -0500

    Implementación inicial del backend de préstamos

---
+++ demo-ceiba-biblioteca/src/main/java/com/example/ceiba/demo_ceiba_biblioteca/DemoCeibaBibliotecaApplication.java
@@ -0,0 +1,13 @@
+package com.example.ceiba.demo_ceiba_biblioteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCeibaBibliotecaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoCeibaBibliotecaApplication.class, args);
+	}
+
+}
---
+++ demo-ceiba-biblioteca/src/test/java/com/example/ceiba/demo_ceiba_biblioteca/DemoCeibaBibliotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ceiba.demo_ceiba_biblioteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCeibaBibliotecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
